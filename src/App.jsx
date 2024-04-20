431175a0283f84b4b2397e3a83565bfe48153dd1
Ankur <annkur020@gmail.com>
2024-04-20 20:32:34 +0530

    pages created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/sign-in",
+    element: <SignIn />,
+  },
+  {
+    path: "/sign-up",
+    element: <SignUp />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+]);
 
 function App() {
-  return (
-    <div>App</div>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
